2dd1e1e9b14bd153a08abe28b67d1311b0cfeca9
Bro-dep <153253648+Bro-dep@users.noreply.github.com>
2024-04-09 00:59:55 -0700

    Added some textures

--- Godot/Scenes/MainMenu.gd
+++ Godot/Scenes/MainMenu.gd
@@ -11,8 +11,11 @@ func _process(_delta):
 	pass
 
 func _on_button_pressed():
+	get_tree().change_scene_to_file("res://Scenes/Level1/Level1.tscn")
+func _on_d_test_pressed():
 	get_tree().change_scene_to_file("res://Scenes/MainScene.tscn")
 
 
 func _on_level_1_pressed():
 	get_tree().change_scene_to_file("res://Scenes/Level1/Level1.tscn")
+
--- Godot/Scenes/pause_menu.gd
+++ Godot/Scenes/pause_menu.gd
@@ -25,3 +25,5 @@ func _on_button_resume_pressed():
 	pauseMenu()
 func _on_button_quit_pressed():
 	get_tree().quit()
+func _on_main_menu_pressed():
+	get_tree().change_scene_to_file("res://Scenes/MainMenu.tscn")
